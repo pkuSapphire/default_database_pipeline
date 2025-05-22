b0eb497cf758cc8711f330146150911f0716001a
Sen <104046465+pkuSapphire@users.noreply.github.com>
2025-05-23 02:22:18 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     description="Pipeline for corporate default analysis using WRDS",
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/yourusername/default_database_pipeline",
+    url="https://github.com/pkuSapphire/default_database_pipeline",
     packages=find_packages(),
     install_requires=[
         "pandas", "numpy", "scikit-learn", "statsmodels", "wrds", "requests"
