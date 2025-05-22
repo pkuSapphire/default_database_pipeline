d8584c9bc4695c475493591eaed734a5cfeeb517
pkuSapphire <104046465+pkuSapphire@users.noreply.github.com>
2025-05-23 02:10:04 +0800

    Initial package structure and core logic

---
+++ setup.py
@@ -0,0 +1,18 @@
+# coding:utf-8
+from setuptools import setup, find_packages
+
+setup(
+    name="default_database_pipeline",
+    version="0.1.0",
+    author="Your Name",
+    author_email="you@example.com",
+    description="Pipeline for corporate default analysis using WRDS",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/default_database_pipeline",
+    packages=find_packages(),
+    install_requires=[
+        "pandas", "numpy", "scikit-learn", "statsmodels", "wrds", "requests"
+    ],
+    python_requires=">=3.7",
+)
